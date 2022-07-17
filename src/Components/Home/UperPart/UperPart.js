c9ccf00230e9f39f8db2e225c103f2e65ca83bb6
mdemon420 <mdeamon4@gmail.com>
2022-07-17 18:42:27 +0600

    backgound color add

--- src/Components/Home/UperPart/UperPart.js
+++ src/Components/Home/UperPart/UperPart.js
@@ -6,18 +6,20 @@ import './UperPart.css';
 
 const UperPart = () => {
     return (
-        <Container className="container">
-            <Row className="d-flex align-items-center">
-                <Col>
-                    <h1>A learning platform that helps you to learning faster</h1>
-                    <p>World-class training and development programs developed by top teachers</p>
-                    <button className="btn-top">Expore All Course</button>
-                </Col>
-                <Col>
-                    <img src={img} alt="" />
-                </Col>
-            </Row>
-        </Container>
+        <div className="body">
+            <Container >
+                <Row className="d-flex align-items-center">
+                    <Col>
+                        <h1>A learning platform that helps you to learning faster</h1>
+                        <p>World-class training and development programs developed by top teachers</p>
+                        <button className="btn-top">Expore All Course</button>
+                    </Col>
+                    <Col>
+                        <img src={img} alt="" />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };
 
