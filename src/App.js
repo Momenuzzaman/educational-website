cdf6855d7b72081ac43b25227b4ffd3faf1e7b1a
mdemon420 <mdeamon4@gmail.com>
2022-07-17 12:39:03 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Home from './Components/Home/Home';
-
+import Footer from './Components/Shared/Footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Shared/Header/Header';
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
 
 const Header = () => {
+
     return (
         <div>
-            <h1>This is Header</h1>
+            <>
+                <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg">
+                    <Container>
+                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                        <Navbar.Toggle />
+                        <Navbar.Collapse className="justify-content-end">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#courses">Courses</Nav.Link>
+                            <Nav.Link href="#blog">Blog</Nav.Link>
+                            <Nav.Link href="#Page">Page</Nav.Link>
+                            <Navbar.Text>
+                                Signed in as: <a href="#login">Mark Otto</a>
+                            </Navbar.Text>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
         </div>
     );
 };
