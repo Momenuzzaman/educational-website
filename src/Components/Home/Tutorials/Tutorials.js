96e4b1c0695d1e274ba500573a8ee19a582ebc1e
mdemon420 <mdeamon4@gmail.com>
2022-07-17 19:14:00 +0600

    data fetch

--- src/Components/Home/Tutorials/Tutorials.js
+++ src/Components/Home/Tutorials/Tutorials.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Tutorials = () => {
-    const [tutorials, setTutorial] = useState({});
+    const [tutorials, setTutorial] = useState([]);
+
+    useEffect(() => {
+        fetch("education.json")
+            .then(res => res.json())
+            .then(data => setTutorial(data));
+    }, []);
     return (
         <div>
-            <h1>This is a tutorials</h1>
+            <h1>This is a tutorials {tutorials.length}</h1>
         </div>
     );
 };
