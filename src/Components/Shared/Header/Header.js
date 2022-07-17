332ff86368b604480b57bf7ca3c27942abeb5fbf
mdemon420 <mdeamon4@gmail.com>
2022-07-17 13:07:35 +0600

    nav form add

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import logo from '../../../image/nav.png';
+import Form from 'react-bootstrap/Form';
+import './Header.css';
 
 
 const Header = () => {
@@ -10,20 +12,26 @@ const Header = () => {
     return (
         <div>
             <>
-                <Navbar bg="light" variant="dark" sticky="top" collapseOnSelect expand="lg">
+                <Navbar bg="light" variant="dark" sticky="top" className="nav" collapseOnSelect expand="lg">
                     <Container>
                         <Navbar.Brand href="#home">
                             <img src={logo} height="34" alt="" />
                         </Navbar.Brand>
                         <Navbar.Toggle />
                         <Navbar.Collapse className="justify-content-end">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#courses">Courses</Nav.Link>
-                            <Nav.Link href="#blog">Blog</Nav.Link>
-                            <Nav.Link href="#Page">Page</Nav.Link>
-                            <Navbar.Text>
-                                Signed in as: <a href="#login">Mark Otto</a>
-                            </Navbar.Text>
+                            <Nav.Link className="nav-font" href="#home">Home</Nav.Link>
+                            <Nav.Link className="nav-font" href="#courses">Courses</Nav.Link>
+                            <Nav.Link className="nav-font" href="#blog">Blog</Nav.Link>
+                            <Nav.Link className="nav-font" href="#Page">Page</Nav.Link>
+                            <Form className="d-flex">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
