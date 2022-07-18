42af1eb8a0a10b30bff5a9eb6d3d6e8af86e8274
mdemon420 <mdeamon4@gmail.com>
2022-07-18 20:27:10 +0600

    Page not found create

---
+++ src/Components/PageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import error from '../../image/error.jpg';
+
+const PageNotFound = () => {
+    return (
+        <div>
+            <img src={error} alt="" />
+        </div>
+    );
+};
+
+export default PageNotFound;
