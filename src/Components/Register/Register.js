f696e77f9152e67acb2f80799453ad58ab40aa4b
mdemon420 <mdeamon4@gmail.com>
2022-07-20 02:26:45 +0600

    register form add

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Register = () => {
     return (
         <div>
-            <h1>This is register</h1>
+
+            <div className="container form">
+                <Form>
+                    <h2>Login</h2>
+                    <br />
+                    <Form.Group className="mb-3" controlId="formGroupEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" className="btn-top login" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
 };
