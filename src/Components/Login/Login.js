0c8b32bb03793eb9c341a1cc789be9ae3aee86ef
mdemon420 <mdeamon4@gmail.com>
2022-07-20 01:30:35 +0600

    add login img

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import img from '../../image/cart.jpeg';
 
 const Login = () => {
     return (
         <div>
+            <img src={img} alt="" />
             <h1>This is login Page</h1>
         </div>
     );
