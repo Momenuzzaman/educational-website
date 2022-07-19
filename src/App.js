82852b4664fcbc589b15be6116b4ad796d54ea8c
mdemon420 <mdeamon4@gmail.com>
2022-07-20 01:19:16 +0600

    react path add

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Tutorials from './Components/Home/Tutorials/Tutorials';
 import Cart from './Components/Cart/Cart';
 import TutorialDetail from './Components/Home/TutorialDetais/TutorialDetail';
 import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
 function App() {
   return (
     <div className="App">
@@ -21,6 +22,7 @@ function App() {
         <Route path="/tutorialDetail/:tutorialId" element={<TutorialDetail />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
