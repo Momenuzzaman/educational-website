20f418db90fe467af86fb984367eb6154eb9b4a8
mdemon420 <mdeamon4@gmail.com>
2022-07-21 02:28:11 +0600

    start and add

--- src/Components/Home/TutorialDetais/TutorialDetail.js
+++ src/Components/Home/TutorialDetais/TutorialDetail.js
@@ -23,25 +23,15 @@ const TutorialDetail = () => {
             </div>
             <div className="container">
                 <p>{description}</p>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={img} />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                    </ListGroup>
-                    <Card.Body>
-                        <Card.Link href="#">Card Link</Card.Link>
-                        <Card.Link href="#">Another Link</Card.Link>
-                    </Card.Body>
-                </Card>
+                <div>
+                    <div>
+                        <div>
+
+                        </div>
+                        <p> You Start on : <span>{start}</span> </p>
+                        <p>Course will end : <span>{end}</span> </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
