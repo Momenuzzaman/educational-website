9eef4db8b24adb81737c8985a6bc6dcf0815f402
mdemon420 <mdeamon4@gmail.com>
2022-07-21 01:18:45 +0600

    import css file

--- src/Components/Home/TutorialDetais/TutorialDetail.js
+++ src/Components/Home/TutorialDetais/TutorialDetail.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import fakeData from '../../../FakeData/Fakedata';
+import './TutorialDetail.css';
 
 const TutorialDetail = () => {
     const { tutorialId } = useParams();
