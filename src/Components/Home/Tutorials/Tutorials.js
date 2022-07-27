03b5f9f13675765328e0519b7dba28e585c56252
mdemon420 <mdeamon4@gmail.com>
2022-07-28 01:05:43 +0600

    export createContext

--- src/Components/Home/Tutorials/Tutorials.js
+++ src/Components/Home/Tutorials/Tutorials.js
@@ -2,31 +2,36 @@
 import Tutorial from '../Tutorial/Tutorial';
 import './Tutorials.css';
 import fakeData from '../../../FakeData/Fakedata';
-import { useState } from 'react';
+import { createContext, useState } from 'react';
+
+export const CartContext = createContext();
 
 const Tutorials = () => {
     const [tutorials, setProducts] = useState(fakeData);
     const [cart, setCart] = useState([]);
 
-    console.log(tutorials);
-    const handleAddToCart = (product) => {
-        console.log(product);
+    const handleAddToCart = (tutorial) => {
+        const newCart = [...cart, tutorial];
+        setCart(newCart);
+        console.log(cart);
     };
-
     return (
-        <div className=" container tutorials" id="courses" >
-            <div className="rated">
+        <CartContext.Provider values={cart}>
+            <div className=" container tutorials" id="courses" >
+                <div className="rated">
+                </div>
+                <div className="row">
+                    {
+                        tutorials.map(tutorial => <Tutorial key={tutorial.id}
+                            tutorial={tutorial}
+                            handleAddToCart={handleAddToCart}
+                        ></Tutorial>)
+                    }
+                </div>
             </div>
-            <div className="row">
-                {
-                    tutorials.map(tutorial => <Tutorial key={tutorial.id}
-                        tutorial={tutorial}
-                        handleAddToCart={handleAddToCart}
-                    ></Tutorial>)
-                }
-            </div>
-        </div>
-    );
+            );
+        </CartContext.Provider>
+    
 };
 
 export default Tutorials;
