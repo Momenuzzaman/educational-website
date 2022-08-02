aa9c22cfcd70fca69750350d053ecfc05bfa78b2
mdemon420 <mdeamon4@gmail.com>
2022-08-02 21:17:55 +0600

    firebase initialize add

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDnv_KEOoTeowJBjfQmkJkE3cMtkqikFcg",
+    authDomain: "education-00.firebaseapp.com",
+    projectId: "education-00",
+    storageBucket: "education-00.appspot.com",
+    messagingSenderId: "102783494824",
+    appId: "1:102783494824:web:8f1066ea3f8a16fe6df09d"
+};
+
+export default firebaseConfig;
