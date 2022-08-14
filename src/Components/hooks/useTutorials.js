1151c5a734428d4d9c13dc30cf018097a542db64
mdemon420 <mdeamon4@gmail.com>
2022-08-15 01:49:48 +0600

    export useTutorials

---
+++ src/Components/hooks/useTutorials.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import fakeData from "../../FakeData/Fakedata";
+
+const useTutorials = () => {
+    const [tutorials, setTutorials] = useState(fakeData);
+    return [tutorials, setTutorials];
+};
+export default useTutorials;
