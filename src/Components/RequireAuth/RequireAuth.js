7a69cd972d870f458d4dea329a2d14ec99bcbf6c
mdemon420 <mdeamon4@gmail.com>
2022-08-15 01:14:37 +0600

    create fake db

--- src/Components/RequireAuth/RequireAuth.js
+++ src/Components/RequireAuth/RequireAuth.js
@@ -3,11 +3,11 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import firebase from '../../firebase/firebase';
 
 const RequireAuth = ({ children }) => {
-    const { user } = firebase();
-    const location = useLocation();
-    if (!user.email) {
-        return (<Navigate to="/login" state={{ from: location }} replace></Navigate >)
-    }
+    // const { user } = firebase();
+    // const location = useLocation();
+    // if (!user.email) {
+    //     return (<Navigate to="/login" state={{ from: location }} replace></Navigate >)
+    // }
     return children;
 
 
