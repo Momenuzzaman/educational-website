e4cc9c9e79d10bbb0f56e2a9a1e3b2621e2338f5
mdemon420 <mdeamon4@gmail.com>
2022-08-16 01:49:02 +0600

    import img

--- src/Components/CartReviewItem/CartReviewItem.js
+++ src/Components/CartReviewItem/CartReviewItem.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const CartReviewItem = ({ tutorial }) => {
-    const { name, price, quantity } = tutorial;
+    const { img, name, price, quantity } = tutorial;
     return (
         <div>
-            <h4>{name}</h4>
-            <p>Price: ${price}</p>
-            <p>Quantity : {quantity}</p>
-
+            <div>
+                <img src={img} alt=''></img>
+            </div>
+            <div></div>
         </div>
     );
 };
