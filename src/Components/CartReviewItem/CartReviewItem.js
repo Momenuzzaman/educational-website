3d7cc7bf55d5c2f425c547c47c10b5df4793792b
mdemon420 <mdeamon4@gmail.com>
2022-08-16 02:32:12 +0600

    add some code

--- src/Components/CartReviewItem/CartReviewItem.js
+++ src/Components/CartReviewItem/CartReviewItem.js
@@ -4,18 +4,20 @@ import './CartReviewItem.css';
 const CartReviewItem = ({ tutorial }) => {
     const { img, name, price, quantity } = tutorial;
     return (
-        <div className="cart-review-item">
-            <div>
-                <img src={img} width="150px" alt=''></img>
-            </div>
-            <div className="cart-review-item-content">
+        <div className="container">
+            <div className="cart-review-item">
                 <div>
-                    <p>{name}</p>
-                    <p>Price : ${price}</p>
-                    <p>Quantity : {quantity}</p>
+                    <img src={img} width="150px" alt=''></img>
                 </div>
-                <div>
-                    <button>Delete</button>
+                <div className="cart-review-item-content">
+                    <div className="detail-item">
+                        <p>{name}</p>
+                        <p>Price : ${price}</p>
+                        <p>Quantity : {quantity}</p>
+                    </div>
+                    <div>
+                        <button>Delete</button>
+                    </div>
                 </div>
             </div>
         </div>
