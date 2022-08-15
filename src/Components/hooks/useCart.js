78dd15fa48f0b17b03683588f906aa854b1d9f30
mdemon420 <mdeamon4@gmail.com>
2022-08-15 19:27:50 +0600

    hook save to quantity

--- src/Components/hooks/useCart.js
+++ src/Components/hooks/useCart.js
@@ -1,3 +1,20 @@
-const useCart = tutorials => {
+import { useState } from "react";
+import { getStoredCart } from "../utilities/fakedb";
 
+const useCart = tutorials => {
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const saveCart = [];
+        for (const id in storedCart) {
+            const addToTutorial = tutorials.find(tutorial => tutorial.id === id);
+            if (addToTutorial) {
+                const quantity = storedCart[id];
+                addToTutorial.quantity = quantity;
+                saveCart.push(addToTutorial);
+            }
+            setCart(saveCart);
+        }
+    }, [tutorials]);
 };
