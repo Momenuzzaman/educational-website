2cb3738f8a384c45ffc67655e8d31502464b9d64
mdemon420 <mdeamon4@gmail.com>
2022-08-15 23:27:02 +0600

    cart-container grid add

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import useCart from '../../hooks/useCart';
 import useTutorials from '../../hooks/useTutorials';
-
 import './Cart.css';
 
 
@@ -10,12 +9,15 @@ const Cart = () => {
     const [cart] = useCart(tutorials);
     console.log(cart);
     return (
-        <div className="container">
-            <h1>{tutorials.length}</h1>
-            <h3>Cart: {cart.length}</h3>
-            <h4>Item Order : </h4>
-            <h4>Quantity :</h4>
-            <h4>Price :</h4>
+        <div className="cart-container">
+            <div>
+                <h1>Review</h1>
+            </div>
+            <div>
+                <h4>Item Order : </h4>
+                <h4>Quantity :</h4>
+                <h4>Price :</h4>
+            </div>
         </div>
     );
 };
